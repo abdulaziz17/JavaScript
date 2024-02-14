ccbfa3d5bd331bf37d25f53f7f68f681f04d4faf
Abdul Aziz <32242329+abdulaziz17@users.noreply.github.com>
2024-02-14 13:37:20 +0000

    variables

---
+++ o1_basics/variables.js
@@ -0,0 +1,12 @@
+const accountID = 144553
+let accountEmail = "abdulaziz@gmail.com"
+var accountPassword = "12345"
+accountCity = "Hyderabad"
+
+// accountID = 144552
+accountEmail = "abdul@gmail.com"
+accountPassword = "54321"
+accountCity = "Hyd"
+
+console.log(accountID);
+console.table([accountID,accountEmail,accountPassword,accountCity])
