3706e14e513873c710dc60b972364408d0424e78
Abdul Aziz <32242329+abdulaziz17@users.noreply.github.com>
2024-02-14 15:00:07 +0000

    conversion

---
+++ o1_basics/conversion.js
@@ -0,0 +1,24 @@
+// let score = "33abc"
+
+// console.log(typeof(score));
+
+// let valueInNumber = Number(score)
+
+// console.log(typeof(valueInNumber));
+// console.log(valueInNumber);
+
+// let aNumber = 33
+// let stringNumber = String(aNumber)
+// console.log(stringNumber)
+// console.log(typeof stringNumber);
+
+// let value = 3
+// let negValue = -value
+// console.log(negValue);
+
+// console.log(+true);
+
+let gameCounter = 100
+console.log(++gameCounter);
+console.log(gameCounter++);
+console.log(gameCounter)
